9be99a476ae0614551e5f10fe81e457ba558c7e1
LedoKun <romamp100@gmail.com>
2017-12-06 22:34:02 +0000

    Change locale to en.US.UTF-8

--- build/root/install.sh
+++ build/root/install.sh
@@ -67,9 +67,9 @@ grep -v xfsprogs) \
 pacman -S awk sed supervisor nano vi ldns moreutils net-tools dos2unix unzip unrar htop jq openssl-1.0 --noconfirm
 
 # set locale
-echo en_GB.UTF-8 UTF-8 > /etc/locale.gen
+echo en_US.UTF-8 UTF-8 > /etc/locale.gen
 locale-gen
-echo LANG="en_GB.UTF-8" > /etc/locale.conf
+echo LANG="en_US.UTF-8" > /etc/locale.conf
 
 # add user "nobody" to primary group "users" (will remove any other group membership)
 usermod -g users nobody
